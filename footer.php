12ba5fb6b3163d4e5c6ce8f053fc79ed29f4550b
utkarshakakade <utkarshakakade195@gmail.com>
2021-08-17 12:15:31 -0600

    First Commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">Made by Utkarsha Kakade <br>
+      The Sparks Foundation Project</div>
+ </div>
---
+++ nav.php
@@ -0,0 +1,45 @@
+<!-- nav section starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>National Bank</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <link rel = "icon" href = "img/logo.jpg" type = "image/x-icon">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+</head>
+
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #dfe4ea;">
+    <div class="container-fluid">
+      <a class="navbar-brand logo" href="index.php"><img src="img/logo.jpg" alt="logo" height=50px width=50px></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+
+        <ul class="navbar-nav">
+          <li class="nav-item btn btn-secondary">
+            <a class="nav-link text-white" href="index.php">Home</a>
+          </li>
+         
+          <li class="nav-item btn btn-secondary">
+            <a class="nav-link link-text text-white" href="contactme.php">Contact Us</a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+  </nav>
